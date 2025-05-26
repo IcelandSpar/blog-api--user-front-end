0cd8f7f2b5db204b9efae7424a16009ff6b12f87
IcySpar <victorasielrodriguez@gmail.com>
2025-05-25 22:35:56 -0700

    feat: add css styles to home page and site icon

--- src/components/views/Home.jsx
+++ src/components/views/Home.jsx
@@ -1,11 +1,19 @@
 import Navbar from './partials/Navbar';
 import styles from '../../styles/Home.module.css';
 
+import workBlog from '../../assets/work_blog.png';
+
 const Home = () => {
   return (
-    <div>
+    <div className={styles.homePage}>
       <Navbar navStyle={styles.navStyle}/>
-      <p>Hello world this is the home page !</p>
+      <main className={styles.homeMainCont}>
+        <section className={styles.imgParaCont}>
+          {/* Image by <a href="https://pixabay.com/users/arivleone-14275976/?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4997565">Arivle One</a> from <a href="https://pixabay.com//?utm_source=link-attribution&utm_medium=referral&utm_campaign=image&utm_content=4997565">Pixabay</a> */}
+          <img className={styles.firstContImg} src={workBlog} alt="bloggers" width='200px'/>
+          <p>Read blogs from our authors!</p>
+        </section>
+      </main>
     </div>
   )
 };
