6524f2b89707811d3d6c7eac03e9378da106edf7
IcySpar <victorasielrodriguez@gmail.com>
2025-06-01 11:55:55 -0700

    feat: fix author heart in comments

--- src/components/views/partials/Comment.jsx
+++ src/components/views/partials/Comment.jsx
@@ -1,28 +1,33 @@
+import { useContext } from 'react';
+
 import styles from '../../../styles/Comments.module.css';
 import { formatRelative } from 'date-fns';
 
 import bookmarkHeart from '../../../assets/bookmark_heart.svg';
 import likeIcon from '../../../assets/thumb_up.svg';
 import dislikeIcon from '../../../assets/thumb_down.svg';
+import UserContext from '../../../UserContext';
 
 
 const Comment = ({comment, blogAuthor}) => {
+  const { isLoggedIn } = useContext(UserContext);
+
   return (
     <li className={styles.listItemCont}>
       <div className={styles.commentTitleUserHeartCont}>
         <h3 className={styles.commentHeader}>{comment.commentTitle}</h3>
         <div className={styles.commenUserAndHeartCont}>
           <p className={styles.commentUser}>{comment.user.username}</p>
-          {!comment.authorHeart ? null : (
+          {!(comment.authorHeartedComments.length > 0) ? null : (
             <div className={styles.authorHeartAndIconCont}>
               <img className={styles.bookmarkHeartIcon} src={bookmarkHeart} alt="author hearted" />
               <p className={styles.commentAuthorHeart}>{`${blogAuthor} loved!`}</p>
             </div>
           )}
-
         </div>
       </div>
       <p>{comment.comment}</p>
+      <p>{console.log(comment)}</p>
       <div className={styles.postedAndLikeCont}>
         <div className={styles.postedTimeStamps}>
           <p>Posted: {formatRelative(comment.createdAt, new Date())}</p>
