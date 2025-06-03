1b5df42c332258e579ca2a4d05bbc3c91dcbdd8b
IcySpar <victorasielrodriguez@gmail.com>
2025-06-03 11:34:31 -0700

    feat: add timers to send request to api and count
    timer on comment like/dislike to send requests
    to api to update when user likes or dislikes
    
    to prevent race conditions

--- src/components/views/partials/Comment.jsx
+++ src/components/views/partials/Comment.jsx
@@ -1,4 +1,4 @@
-import { useState, useContext } from 'react';
+import { useState, useContext, useRef } from 'react';
 
 import styles from '../../../styles/Comments.module.css';
 import { formatRelative } from 'date-fns';
@@ -11,43 +11,83 @@ import UserContext from '../../../UserContext';
 
 const Comment = ({comment, blogAuthor}) => {
   const { isLoggedIn } = useContext(UserContext);
+
+  const updateCountTimerInst = useRef({timer: 3});
+  const timerInstance = useRef({timer: 0});
+
   const [ like, setLike ] = useState(comment.UserLikedComments.length);
   const [ dislike, setDislike ] = useState(comment._count.UserLikedComments);
   const [ userCurrentLikeStatus, setUserCurrentLikeStatus ] = useState(null);
+  const [ updateCount, setUpdateCount ] = useState(null);
 
-  const handleLike = (e) => {
+  const sendCurrentLike = (currentLikeStatus, commentId) => {
+    clearTimeout(timerInstance.current.timer)
+    clearInterval(updateCountTimerInst.current.timer);
+
+    setUpdateCount(3);
+    let virtualTime = 3;
+
+    timerInstance.current.timer = setTimeout(() => {
+      console.log(currentLikeStatus)
+      console.log(commentId)
+
+    }, 3000)
+
+    updateCountTimerInst.current.timer = setInterval(() => {
+      if(virtualTime > 1) {
+        virtualTime = virtualTime - 1;
+        setUpdateCount((prev) => prev - 1);
+      } else {
+        setUpdateCount(null)
+      }
+    }, 1000);
+
+
+  }
+
+  const handleLike = (e, commentId) => {
     e.preventDefault();
+    let currentLikeState = null;
     if(isLoggedIn) {
       if(userCurrentLikeStatus == null) {
         setLike((prev) => prev + 1)
         setUserCurrentLikeStatus(true);
+        currentLikeState = true;
       } else if(userCurrentLikeStatus == false) {
         setDislike((prev) => prev - 1)
         setLike((prev) => prev + 1);
         setUserCurrentLikeStatus(true);
+        currentLikeState = true;
       } else if (userCurrentLikeStatus == true) {
         setLike((prev) => prev - 1);
         setUserCurrentLikeStatus(null);
+        currentLikeState = null;
       }
     }
 
+    sendCurrentLike(currentLikeState, commentId);
   };
 
-  const handleDislike = (e) => {
+  const handleDislike = (e, commentId) => {
     e.preventDefault();
+    let currentLikeState = null;
     if(isLoggedIn) {
       if(userCurrentLikeStatus == null) {
         setDislike((prev) => prev + 1);
         setUserCurrentLikeStatus(false);
+        currentLikeState = false;
       } else if (userCurrentLikeStatus == true) {
         setLike((prev) => prev - 1);
         setDislike((prev) => prev + 1);
         setUserCurrentLikeStatus(false);
+        currentLikeState = false;
       } else if (userCurrentLikeStatus == false) {
         setDislike((prev) => prev - 1);
         setUserCurrentLikeStatus(null);
+        currentLikeState = null;
       }
     }
+    sendCurrentLike(currentLikeState, commentId);
   };
 
   return (
@@ -72,15 +112,16 @@ const Comment = ({comment, blogAuthor}) => {
         }
         </div>
         <div className={styles.commentLikeAndDislikeCont}>
-          <button onClick={handleLike} type='button' className={`${styles.likeAndParaBtn} ${userCurrentLikeStatus == true ? styles.activeCommentLike : null}`}>
+          <button onClick={(e) => handleLike(e, comment.id)} type='button' className={`${styles.likeAndParaBtn} ${userCurrentLikeStatus == true ? styles.activeCommentLike : null}`}>
             <img className={styles.likeIcon} src={likeIcon} alt="like" />
             <p>{like}</p>
           </button>
-          <button onClick={handleDislike} type='button' className={`${styles.likeAndParaBtn} ${userCurrentLikeStatus == false ? styles.activeCommentLike : null}`}>
+          <button onClick={(e) => handleDislike(e, comment.id)} type='button' className={`${styles.likeAndParaBtn} ${userCurrentLikeStatus == false ? styles.activeCommentLike : null}`}>
             <img className={styles.likeIcon} src={dislikeIcon} alt="dislike" />
             <p>{dislike}</p>
           </button>
         </div>
+        <p>{updateCount}</p>
       </div>
     </li>
   )
